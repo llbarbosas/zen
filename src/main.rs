69f93c4e8df6cd7327673ebc5e5aa4a2e796b79d
Lucas Barbosa <llbarbosasc@gmail.com>
2020-07-19 02:21:52 -0400

    feat: Node type

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,70 @@ enum Align {
 }
 
 #[derive(Clone)]
+struct Spacing {
+    top: f64,
+    left: f64,
+    bottom: f64,
+    right: f64,
+}
+
+impl Spacing {
+    fn all(value: f64) -> Self {
+        Spacing {
+            top: value,
+            left: value,
+            right: value,
+            bottom: value,
+        }
+    }
+
+    fn top(top: f64) -> Self {
+        Spacing {
+            top,
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+        }
+    }
+
+    fn left(left: f64) -> Self {
+        Spacing {
+            top: 0.0,
+            left,
+            right: 0.0,
+            bottom: 0.0,
+        }
+    }
+
+    fn right(right: f64) -> Self {
+        Spacing {
+            top: 0.0,
+            left: 0.0,
+            right,
+            bottom: 0.0,
+        }
+    }
+
+    fn bottom(bottom: f64) -> Self {
+        Spacing {
+            top: 0.0,
+            left: 0.0,
+            right: 0.0,
+            bottom,
+        }
+    }
+
+    fn tlrb(top: f64, left: f64, right: f64, bottom: f64) -> Self {
+        Spacing {
+            top,
+            left,
+            right,
+            bottom,
+        }
+    }
+}
+
+#[derive(Clone)]
 struct Node {
     direction: Direction,
     basis: Basis,
@@ -59,6 +123,9 @@ struct Node {
     min_width: f64,
     min_height: f64,
     aspect_ratio: f64,
+    padding: Spacing,
+    margin: Spacing,
+    border: Spacing,
     children: Box<[Node]>,
 }
 
@@ -81,37 +148,108 @@ impl Node {
             min_width: 0.0,
             min_height: 0.0,
             aspect_ratio: 0.0,
+            padding: Spacing::all(0.0),
+            margin: Spacing::all(0.0),
+            border: Spacing::all(0.0),
             children: Box::from([]),
         }
     }
 
-    fn set_width(self, width: f64) -> Self {
-        Node { width, ..self }
+    fn direction(mut self, direction: Direction) -> Self {
+        self.direction = direction;
+        self
     }
 
-    fn set_height(self, height: f64) -> Self {
-        Node { height, ..self }
+    fn basis(mut self, basis: Basis) -> Self {
+        self.basis = basis;
+        self
     }
 
-    fn set_direction(self, direction: Direction) -> Self {
-        Node { direction, ..self }
+    fn grow(mut self, grow: f32) -> Self {
+        self.grow = grow;
+        self
     }
 
-    fn set_basis(self, basis: Basis) -> Self {
-        Node { basis, ..self }
+    fn shrink(mut self, shrink: f32) -> Self {
+        self.shrink = shrink;
+        self
     }
 
-    fn set_grow(self, grow: f32) -> Self {
-        Node { grow, ..self }
+    fn wrap(mut self, wrap: Wrap) -> Self {
+        self.wrap = wrap;
+        self
     }
 
-    fn insert_child(self, child: Self) -> Self {
-        Node {
-            width: self.width + child.width,
-            height: self.height + child.height,
-            children: Box::from([self.children, Box::from([child])].concat()),
-            ..self
-        }
+    fn justify_content(mut self, justify_content: JustifyContent) -> Self {
+        self.justify_content = justify_content;
+        self
+    }
+
+    fn align_items(mut self, align_items: Align) -> Self {
+        self.align_items = align_items;
+        self
+    }
+
+    fn align_self(mut self, align_self: Align) -> Self {
+        self.align_self = align_self;
+        self
+    }
+
+    fn align_content(mut self, align_content: Align) -> Self {
+        self.align_content = align_content;
+        self
+    }
+
+    fn width(mut self, width: f64) -> Self {
+        self.width = width;
+        self
+    }
+
+    fn height(mut self, height: f64) -> Self {
+        self.height = height;
+        self
+    }
+
+    fn max_width(mut self, max_width: f64) -> Self {
+        self.max_width = max_width;
+        self
+    }
+
+    fn max_height(mut self, max_height: f64) -> Self {
+        self.max_height = max_height;
+        self
+    }
+
+    fn min_width(mut self, min_width: f64) -> Self {
+        self.min_width = min_width;
+        self
+    }
+
+    fn aspect_ratio(mut self, aspect_ratio: f64) -> Self {
+        self.aspect_ratio = aspect_ratio;
+        self
+    }
+
+    fn padding(mut self, padding: Spacing) -> Self {
+        self.padding = padding;
+        self
+    }
+
+    fn margin(mut self, margin: Spacing) -> Self {
+        self.margin = margin;
+        self
+    }
+
+    fn border(mut self, border: Spacing) -> Self {
+        self.border = border;
+        self
+    }
+
+    fn insert_child(mut self, child: Self) -> Self {
+        self.width += child.width;
+        self.height += child.height;
+        self.children = Box::from([self.children, Box::from([child])].concat());
+        self
     }
 }
 
@@ -128,8 +266,8 @@ impl std::fmt::Display for Node {
 }
 
 fn main() {
-    let node1 = Node::new().set_width(100.0).set_height(100.0);
-    let node2 = Node::new().set_width(300.0).set_height(400.0);
+    let node1 = Node::new().width(100.0).height(100.0);
+    let node2 = Node::new().width(300.0).height(400.0);
 
     let root = Node::new().insert_child(node1).insert_child(node2);
 
